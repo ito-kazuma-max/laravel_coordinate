dee92b8c59f38985065bd1af0d4085fc1fb41c70
伊藤和馬 <itoukazuma@mbp.local>
2022-03-14 20:10:57 +0900

    マイページ修正

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -6,22 +6,37 @@
 <h1 class="mb-5">{{ $title }}</h1>
 
 
-
-<div class="row mt-4">
-
-    <a href="{{ route('items.create') }}" class="btn btn-outline-dark col-4 offset-1 pt-5 pb-5 mb-5">新規アイテム<br>登録</a>
-
-    <a href="{{ route('items.index') }}" class="btn btn-secondary col-4 offset-1 pt-5 pb-5 mb-5">アイテム<br>一覧</a>
-
-    <a href="{{ route('coordinates.create', $user) }}" class="btn btn-outline-primary col-4 offset-1 pt-5 pb-5 mb-5">新規コーデ<br>登録</a>
-
-    <a href="{{ route('coordinates.index', $user) }}" class="btn btn-info col-4 offset-1 pt-5 pb-5 mb-5">マイコーデ<br>一覧</a>
-
-    <div class="col-12 text-center">
-        <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://max-ito.com/coorde-for/users/{{ $user->id }}/user_page" data-color="default" data-size="large" data-count="false" data-ver="3" style="display: none;"></div>
+<div class="row justify-content-center">
+    <div class="col-8 col-lg-6 mt-4 mb-3">
+        <div class="card bg-light border-secondary">
+            <div class="card-header border-secondary">
+                アイテム
+            </div>
+            <div class="card-body">
+                <a href="{{ route('items.create') }}" class="btn btn-secondary pt-2 pb-2 mb-3 w-100">登録する</a>
+                <a href="{{ route('items.index') }}" class="btn btn-secondary pt-2 pb-2 mb-2 w-100">一覧を見る</a>
+            </div>
+        </div>
     </div>
 
+    <div class="col-8 col-lg-6 mt-4 mb-3">
+        <div class="card bg-light border-info">
+            <div class="card-header text-primary border-info">
+                マイコーデ
+            </div>
+            <div class="card-body">
+                <a href="{{ route('coordinates.create', $user) }}" class="btn btn-primary pt-2 pb-2 mb-3 w-100">登録する</a>
+                <a href="{{ route('coordinates.index', $user) }}" class="btn btn-primary pt-2 pb-2 mb-2 w-100">一覧を見る</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center mt-4">
+         <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://max-ito.com/coorde-for/users/{{ $user->id }}/user_page" data-color="default" data-size="large" data-count="false" data-ver="3" style="display: none;"></div>
+     </div>
 </div>
+
+
+
 
 <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
 
