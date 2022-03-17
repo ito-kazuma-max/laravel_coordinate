40a9ade81f05ea19fd6e7e1da7d21e47de6319dd
伊藤和馬 <itoukazuma@mbp.local>
2022-03-17 13:42:33 +0900

    twitter連携

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 </head>
 <body class="d-flex flex-column">
     <header>
@@ -67,7 +68,7 @@
         @endguest
     </header>
 
-    <div class="container">
+    <div class="container mb-5">
 
         @foreach($errors->all() as $error)
             <p class="error">{{ $error }}</p>
--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -30,14 +30,21 @@
             </div>
         </div>
     </div>
-    <div class="col-12 text-center mt-4">
-         <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://max-ito.com/coorde-for/users/{{ $user->id }}/user_page" data-color="default" data-size="large" data-count="false" data-ver="3" style="display: none;"></div>
+    <div class="col-8 mt-5 text-center">
+        <p>コーディネートを提案してもらおう！
+    </div>
+    <div class="col-6 mt-2 text-right">
+        <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://max-ito.com/coorde-for/users/{{ $user->id }}/user_page" data-color="default" data-size="large" data-count="false" data-ver="3" style="display: none;"></div>
      </div>
+    <div class="col-6 mt-2 pl-4">
+       <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="私をコーデして！&#13" data-url="https://max-ito.com/coorde-for/users/{{ $user->id }}/user_page&#13" data-hashtags="coorde_for" data-size="large" data-show-count="false">Tweet</a>
+    </div>
+
 </div>
 
 
 
-
 <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 @endsection
